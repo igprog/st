1d9e87b37cc49ee8c8ad7ab236341dec5be219ee
Igor Gašparović <dragan.stanojevic@3cad.hr>
2019-11-24 20:08:54 +0100

    Reservation.

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,33 @@ app.js
 */
 angular.module('app', [])
 
+.factory('f', ['$http', ($http) => {
+    return {
+        post: (service, method, data) => {
+            return $http({
+                url: './' + service + '.asmx/' + method,
+                method: 'POST',
+                data: data
+            })
+            .then((response) => {
+                return JSON.parse(response.data.d);
+            },
+            (response) => {
+                return response.data.d;
+            });
+        },
+        setDate: (x) => {
+            var day = x.getDate();
+            day = day < 10 ? '0' + day : day;
+            var mo = x.getMonth();
+            mo = mo + 1 < 10 ? '0' + (mo + 1) : mo + 1;
+            var yr = x.getFullYear();
+            return yr + '-' + mo + '-' + day;
+        }
+    }
+}])
+
+
 .controller('appCtrl', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
 
     var getConfig = function () {
@@ -16,7 +43,28 @@ angular.module('app', [])
     $scope.year = (new Date).getFullYear();
 }])
 
+.controller('reservationCtrl', ['$scope', '$http', 'f', function ($scope, $http, f) {
+    var webService = 'Mail';
+    var d = {
+        service: null,
+        date: new Date(),
+        time: null,
+        name: null,
+        phone: ''
+    }
+    $scope.d = d;
+
+    $scope.send = function (d) {
+        alert('TODO'); return false;
+        f.post(webService, 'SendReservation', { x: d }).then((d) => {
+            //alert(d.msg);
+        });
+    }
+
+}])
+
 .controller('contactCtrl', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
+    var webService = 'Mail';
     $scope.showAlert = false;
     $scope.sendicon = 'far fa-envelope';
     $scope.sendicontitle = 'Pošalji';
@@ -31,27 +79,35 @@ angular.module('app', [])
         $scope.isSendButtonDisabled = true;
         $scope.sendicon = 'fa fa-spinner fa-spin';
         $scope.sendicontitle = 'Šaljem';
-        $http({ url: '../Mail.asmx/Send', method: 'POST',
-            data: { name: d.name, email: d.email, messageSubject: 'Upit', message: d.message }
-        }).then(function (response) {
-           if (response.data.d == 'sent') {
-               $scope.showAlert = true;
-               $scope.sendicon = 'far fa-envelope';
-               $scope.sendicontitle = 'Pošalji';
-               window.location.hash = 'contact';
-           } else {
-               $scope.showAlert = false;
-               $scope.sendicon = 'far fa-envelope';
-               $scope.sendicontitle = 'Pošalji';
-               alert(response.data.d);
-           }
-       },
-       function (response) {
-           $scope.showAlert = false;
-           $scope.sendicon = 'far fa-envelope';
-           $scope.sendicontitle = 'Pošalji';
-           alert(response.data.d);
-       });
+
+        f.post(webService, 'Send', { name: d.name, email: d.email, messageSubject: 'Upit', message: d.message }).then((d) => {
+            //alert(d.msg);
+        })
+
+       // $http({
+       //     url: '../Mail.asmx/Send', method: 'POST',
+       //     data: { name: d.name, email: d.email, messageSubject: 'Upit', message: d.message }
+       // }).then(function (response) {
+       //    if (response.data.d == 'sent') {
+       //        $scope.showAlert = true;
+       //        $scope.sendicon = 'far fa-envelope';
+       //        $scope.sendicontitle = 'Pošalji';
+       //        window.location.hash = 'contact';
+       //    } else {
+       //        $scope.showAlert = false;
+       //        $scope.sendicon = 'far fa-envelope';
+       //        $scope.sendicontitle = 'Pošalji';
+       //        alert(response.data.d);
+       //    }
+       //},
+       //function (response) {
+       //    $scope.showAlert = false;
+       //    $scope.sendicon = 'far fa-envelope';
+       //    $scope.sendicontitle = 'Pošalji';
+       //    alert(response.data.d);
+       //});
+
+
     }
 
 }])
