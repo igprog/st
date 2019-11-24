7ea8fbd379711734c6ee84df8c3f118d3a939e80
Igor Gašparović <dragan.stanojevic@3cad.hr>
2019-11-24 15:12:48 +0100

    First commit.

---
+++ App_Code/Mail.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using System.Net;
+using System.Net.Mail;
+using System.Configuration;
+using System.Text;
+
+/// <summary>
+/// SendMail
+/// </summary>
+[WebService(Namespace = "http://igprog.hr/")]
+[WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+[System.Web.Script.Services.ScriptService]
+public class Mail : System.Web.Services.WebService {
+    string myEmail = ConfigurationManager.AppSettings["myEmail"];
+    string myEmailName = ConfigurationManager.AppSettings["myEmailName"];
+    string myPassword = ConfigurationManager.AppSettings["myPassword"];
+    int myServerPort = Convert.ToInt32(ConfigurationManager.AppSettings["myServerPort"]);
+    string myServerHost = ConfigurationManager.AppSettings["myServerHost"];
+
+    public Mail() {
+    }
+
+    public string SendMail(string sendTo, string messageSubject, string messageBody) {
+        try {
+            MailMessage mailMessage = new MailMessage();
+            SmtpClient Smtp_Server = new SmtpClient();
+            Smtp_Server.UseDefaultCredentials = false;
+            Smtp_Server.Credentials = new NetworkCredential(myEmail, myPassword);
+            Smtp_Server.Port = myServerPort;
+            Smtp_Server.EnableSsl = true;
+            Smtp_Server.Host = myServerHost;
+            mailMessage.To.Add(sendTo);
+            mailMessage.From = new MailAddress(myEmail, myEmailName);
+            mailMessage.Subject = messageSubject;
+            mailMessage.Body = messageBody;
+            mailMessage.IsBodyHtml = true;
+            Smtp_Server.Send(mailMessage);
+            return "sent";
+        } catch (Exception e) {
+            return e.Message;
+        }
+    }
+
+    [WebMethod]
+    public string Send(string name, string email, string messageSubject, string message) {
+       string messageBody = string.Format(
+@"
+<hr>Novi upit</h3>
+<p>Ime: {0}</p>
+<p>Email: {1}</p>
+<p>Poruka: {2}</p>", name, email, message);
+        return SendMail(myEmail, messageSubject, messageBody);
+    }
+
+}
---
+++ assets/js/app.js
@@ -0,0 +1,60 @@
+﻿/*!
+app.js
+(c) 2019 IG PROG, www.igprog.hr
+*/
+angular.module('app', [])
+
+.controller('appCtrl', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
+
+    var getConfig = function () {
+        $http.get('../config/config.json')
+          .then(function (response) {
+              $rootScope.config = response.data;
+          });
+    };
+    getConfig();
+    $scope.year = (new Date).getFullYear();
+}])
+
+.controller('contactCtrl', ['$scope', '$http', '$rootScope', function ($scope, $http, $rootScope) {
+    $scope.showAlert = false;
+    $scope.sendicon = 'far fa-envelope';
+    $scope.sendicontitle = 'Pošalji';
+
+    $scope.d = {
+        name: '',
+        email: '',
+        message: ''
+    }
+
+    $scope.send = function (d) {
+        $scope.isSendButtonDisabled = true;
+        $scope.sendicon = 'fa fa-spinner fa-spin';
+        $scope.sendicontitle = 'Šaljem';
+        $http({ url: '../Mail.asmx/Send', method: 'POST',
+            data: { name: d.name, email: d.email, messageSubject: 'Upit', message: d.message }
+        }).then(function (response) {
+           if (response.data.d == 'sent') {
+               $scope.showAlert = true;
+               $scope.sendicon = 'far fa-envelope';
+               $scope.sendicontitle = 'Pošalji';
+               window.location.hash = 'contact';
+           } else {
+               $scope.showAlert = false;
+               $scope.sendicon = 'far fa-envelope';
+               $scope.sendicontitle = 'Pošalji';
+               alert(response.data.d);
+           }
+       },
+       function (response) {
+           $scope.showAlert = false;
+           $scope.sendicon = 'far fa-envelope';
+           $scope.sendicontitle = 'Pošalji';
+           alert(response.data.d);
+       });
+    }
+
+}])
+
+
+;
